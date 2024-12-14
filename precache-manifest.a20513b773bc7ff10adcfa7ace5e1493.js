03356d0fb24264dbfb61362ea14a5e9643a2536f
EyadFageera <eyad.ahf@gmail.com>
2024-12-14 18:58:50 +0300

    Updates

---
+++ precache-manifest.a20513b773bc7ff10adcfa7ace5e1493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1787f2e87b8bf4050b650218a5274a",
+    "url": "/cpit-405-FinalLab-bookmarkingApp/index.html"
+  },
+  {
+    "revision": "782e9dbceacc48b274a7",
+    "url": "/cpit-405-FinalLab-bookmarkingApp/static/css/main.25a537b8.chunk.css"
+  },
+  {
+    "revision": "4699c88a6e8558f87d4e",
+    "url": "/cpit-405-FinalLab-bookmarkingApp/static/js/2.7ab0cd01.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/cpit-405-FinalLab-bookmarkingApp/static/js/2.7ab0cd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8bfa8bff3dad745f1c6",
+    "url": "/cpit-405-FinalLab-bookmarkingApp/static/js/3.c93c3b3c.chunk.js"
+  },
+  {
+    "revision": "782e9dbceacc48b274a7",
+    "url": "/cpit-405-FinalLab-bookmarkingApp/static/js/main.faa655cf.chunk.js"
+  },
+  {
+    "revision": "bdc3d4930cf352111fed",
+    "url": "/cpit-405-FinalLab-bookmarkingApp/static/js/runtime-main.8462ca3a.js"
+  }
+]);
